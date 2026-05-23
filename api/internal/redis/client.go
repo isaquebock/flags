4b84097a0e4c331c3ed61db5a6fab90b03863de0
Isaque Böck <isaquebockdos@gmail.com>
2026-05-23 00:34:12 -0300

    feat(api): add Redis client with health ping

---
+++ api/internal/redis/client.go
@@ -0,0 +1,25 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewClient(addr, password string) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       0,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
